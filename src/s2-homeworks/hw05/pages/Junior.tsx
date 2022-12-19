ff6a674c8f3d0c6cb0a422eeafcfdedb1e5ef6fa
georgkiim <kimkim99975@gmail.com>
2022-12-19 16:19:48 +0300

    hw2 finished 3 hw

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 function Junior() {
     return (
         <div id={'hw5-page-junior'}>
+             junior page
             {/*<HW6 />*/}
             {/*<HW7 />*/}
             {/*<HW8 />*/}

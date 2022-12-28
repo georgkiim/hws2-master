ba1ec28eb3363884ca0c673f791d1c24e7896e3d
georgkiim <kimkim99975@gmail.com>
2022-12-28 19:50:19 +0300

    complete homework 8!!!

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,47 @@ type ActionType =
 export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return action.payload === 'up' ? [...state.sort(function (a: UserType, b: UserType) {
+                    {
+                        if (a.name > b.name) {
+                            return 1;
+                        }
+                        if (a.name < b.name) {
+                            return -1;
+                        }
+                        // a должно быть равным b
+                        return 0;
+                    }
+                }
+            )] : [...state.sort(function (a: UserType, b: UserType) {
+                    {
+                        if (a.name > b.name) {
+                            return -1;
+                        }
+                        if (a.name < b.name) {
+                            return 1;
+                        }
+                        // a должно быть равным b
+                        return 0;
+                    }
+                }
+            )] // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            const stateSortAge = [...state.sort(function (a: UserType, b: UserType) {
+                    if (action.payload === 'up') {
+                        if (a.age > b.age) {
+                            return 1;
+                        }
+                        if (a.age < b.age) {
+                            return -1;
+                        }
+                        // a должно быть равным b
+                        return 0;
+                    }
+                }
+            )]
+            return stateSortAge.filter(t=> t.age>18) // need to fix
         }
         default:
             return state
